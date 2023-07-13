00b81eaf7d0bf5b1190ef91e6ae46c0871f6d567
Fabricio Nascimento <fabriciocunha040@gmail.com>
2023-07-13 17:35:08 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import About from "./assets/components/About/About";
+import Home from "./assets/components/Home/Home";
+
+export default function App() {
+
+  return (
+    <>
+     <Home/>
+    <About />
+    </>
+  )
+}
