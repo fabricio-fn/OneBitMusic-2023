137009a3c52bb18371a34bfd08f42a726087aa78
Fabricio Nascimento <fabriciocunha040@gmail.com>
2023-07-15 17:51:40 -0300

    Add Section Event

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import About from "./assets/components/About/About";
 import Artists from "./assets/components/Artists/Artists";
+import Event from "./assets/components/Event/Event";
 import Home from "./assets/components/Home/Home";
 
 export default function App() {
 
   return (
     <>
-     <Home/>
-    <About />
-    <Artists />
+     {/* <Home/> */}
+      {/* <About /> */}
+      {/* <Artists /> */}
+      <Event />
     </>
   )
 }
