c00d7a585adf007c4155460951769821b32eba21
Fabricio Nascimento <fabriciocunha040@gmail.com>
2023-07-18 17:32:15 -0300

    tickets and sponsors section finished

---
+++ src/assets/components/Sponsors/Sponsors.jsx
@@ -0,0 +1,24 @@
+import "./sponsors.scss"
+
+export default function Sponsors() {
+    return (
+        <section className="sponsors">
+            <h3>Nossos investidores</h3>
+            
+            <div className="logo">
+                <img src="../../public/img/googleLogo.svg" alt="" srcset="" />
+                <img src="../../public/img/microsoftLogo.svg" alt="" srcset="" />
+                <img src="../../public/img/airbnbLogo.svg" alt="" srcset="" />
+                <img src="../../public/img/axureLogo.svg" alt="" srcset="" />
+                <img src="../../public/img/appleLogo.svg" alt="" srcset="" />
+                <img src="../../public/img/facebookLogo.svg" alt="" srcset="" />
+                <img src="../../public/img/nokiaLogo.svg" alt="" srcset="" />
+                <img src="../../public/img/oracleLogo.svg" alt="" srcset="" />
+            </div>
+
+            <div>
+                <button>Se torne um investidor</button>
+            </div>
+        </section>
+    )
+}
---
+++ src/assets/components/Tickets/Cards/Cards.jsx
@@ -0,0 +1,25 @@
+import "./cards.scss";
+
+export default function Cards({ title, R$, p1, p2, p3, button, cardStyle }) {
+
+    const cardClassName = ` cardItem ${cardStyle}`;
+
+    return (
+        <div className={cardClassName}>
+            <div className="tittle">
+                <h3>{ title }</h3>
+                <h4>{ R$ }</h4>
+            </div>
+
+            <div className="content">
+                <p>{ p1 }</p>
+                <p>{ p2 }</p>
+                <p>{ p3 }</p>
+            </div>
+
+            <div className="button">
+                <button>{ button }</button>
+            </div>
+        </div>
+    )
+}
