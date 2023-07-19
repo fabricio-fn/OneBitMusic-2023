ce6883ada35870f7933f127b775a4564c7aa298c
Fabricio Nascimento <fabriciocunha040@gmail.com>
2023-07-19 18:03:30 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import About from "./assets/components/About/About";
 import Artists from "./assets/components/Artists/Artists";
 import Event from "./assets/components/Event/Event";
+import Gallery from "./assets/components/Gallery/Gallery";
 import Home from "./assets/components/Home/Home";
+import Sponsors from "./assets/components/Sponsors/Sponsors";
+import Testimonials from "./assets/components/Testimonials/Testimonials";
 import Tickets from "./assets/components/Tickets/Tickets";
 
 export default function App() {
 
   return (
     <>
-      {/* <Home/> */}
-      {/* <About /> */}
-      {/* <Artists /> */}
-      {/* <Event /> */}
+      {/* <Home/>
+      <About />
+      <Artists />
+      <Event />
       <Tickets />
+      <Sponsors /> 
+      <Testimonials /> */}
+      <Gallery />
     </>
   )
 }
--- src/assets/components/Tickets/Tickets.jsx
+++ src/assets/components/Tickets/Tickets.jsx
@@ -1,9 +1,43 @@
+import Cards from "./Cards/Cards"
 import "./tickets.scss"
 
 export default function Tickets() {
     return (
         <section className="tickets">
-            <h3>Em andamento</h3>
+            <div className="tittle">
+                <h2>Escolha o tipo de ingresso</h2>
+            </div>
+            
+            <div className="cards">
+                <Cards
+                    title="Basico"
+                    R$="R$100"
+                    p1="Acesso aos shows digitais"
+                    p2="Experiência musical imersiva"
+                    p3="Preço acessivel"
+                    button="Saber mais"
+                />
+
+                <Cards
+                    title="Padrão"
+                    R$="R$150"
+                    p1="Acesso aos shows digitais"
+                    p2="Beneficios Extras"
+                    p3="Possibilidade de interagir com os artistas"
+                    button="Saber mais"
+                    cardStyle="padrao"
+                />
+
+                <Cards
+                    title="VIP"
+                    R$="R$200"
+                    p1="Acesso aos shows digitais"
+                    p2="Vantagens adicionais"
+                    p3="Experiência VIP"
+                    button="Saber mais"
+                    cardStyle="vip"
+                />
+            </div>
         </section>
     )
 }
